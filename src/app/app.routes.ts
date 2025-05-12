14b0e89eb511a0bc31723582e57171c37898258a
klauus91 <bakri.tarek@outlook.com>
2025-05-13 00:15:27 +0200

    adding login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {AdminComponent} from './admin/admin.component';
 import {MainDashboardComponent} from './main-dashboard/main-dashboard.component';
+import {LoginComponent} from './auth/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: MainDashboardComponent },       // Main dashboard
   { path: 'atmLiquidity', component: DashboardComponent },       // Main dashboard
-  { path: 'admin', component: AdminComponent },        // Admin page
+  { path: 'admin', component: AdminComponent }, // Admin page
+  { path: 'login', component: LoginComponent }, // Admin page
   { path: '**', redirectTo: '' } // fallback for unknown routes
 ];
