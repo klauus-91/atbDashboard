4f41bb715030302d67b53291886f10de053dccc9
klauus91 <bakri.tarek@outlook.com>
2025-05-13 00:35:18 +0200

    adding footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {NavbarComponent} from './navbar/navbar.component';
 import {SettingsComponent} from './settings/settings.component';
 import {filter} from 'rxjs';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, SettingsComponent],
+  imports: [RouterOutlet, NavbarComponent, SettingsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
